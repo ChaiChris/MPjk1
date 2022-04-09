ae9389a0e4e6f60d7aa7360a36b08ab7e9ee331c
Chris Chai <chris142852145@gmail.com>
2022-04-09 21:13:17 +0800

    Initial commit

---
+++ src/main/java/com/cyt/Hello.java
@@ -0,0 +1,10 @@
+package com.cyt;
+
+public class Hello {
+    public static void main(String[] args) {
+    //    System.out.println("Hello Wrold");
+    Person p = new Person();
+    p.hello();
+    }
+}
+
