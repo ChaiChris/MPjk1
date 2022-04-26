e5263f3142e201a371b1d50b9bfac133586bfcf1
Chris Chai <chris142852145@gmail.com>
2022-04-26 14:12:19 +0800

    Java & Kotlin variable,data type

--- src/main/java/com/cyt/Hello.java
+++ src/main/java/com/cyt/Hello.java
@@ -5,6 +5,15 @@ public class Hello {
     //    System.out.println("Hello Wrold");
     Person p = new Person();
     p.hello();
+    int age = 21;
+    char c = '我';
+    byte b = 120;
+    float weight = 70.3f;
+    float height = 1.7f;
+    boolean adult = true;
+    boolean enroll = false;
+    String name = "Chris";
+
     }
 }
 
--- src/main/kotlin/com/cyt/Hello.kt
+++ src/main/kotlin/com/cyt/Hello.kt
@@ -5,6 +5,13 @@ fun main() {
     //Human().hello()
     val h = Human()
     h.hello()
+    //var ＋ 變數 ＋ ：指定型態（一般不需指定，系統會自動選擇）
+    //var age : Int = 21
+    var age = 20
+    age = 21
+    var weight = 70.3
+    val name : String
+    name = "Chris"
 }
 
 class Human {
